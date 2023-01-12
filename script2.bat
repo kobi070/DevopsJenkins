a14c516bf4682ac89e4bde651ab45d45d0d8e489
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 04:22:02 +0200

    Update script2.bat

--- script2.bat
+++ script2.bat
@@ -1,3 +1,3 @@
 @echo off
-move Date.jsp C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT\KobiKuzi
+xcopy Date.jsp C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT\KobiKuzi
 @echo on
