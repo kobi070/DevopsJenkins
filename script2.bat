a4527a83f9cee9c5f7511633f2bd74d2313bbc91
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 03:59:15 +0200

    Update script2.bat

--- script2.bat
+++ script2.bat
@@ -1,17 +1,9 @@
 # move to JOB_6_GIT_1
 cd C:/Users/Kobi/.jenkins/workspace/JOB_6_GIT_1
 
-# print route
-echo %cd%
-
-# show dir before we copy our file
-dir
-
 # copy Date.jsp
 copy C:/Users/Kobi/.jenkins/workspace/JOB_6_GIT_1/Date.jsp C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT/KobiKuzi
 
-# show new file in dir
-dir
+cd C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT/KobiKuzi
 
-# print route
-echo %cd%
+dir
