0acd833bbf1fdcc39543f1a34ad1af37acc314fa
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 04:44:19 +0200

    Update script2.bat

--- script2.bat
+++ script2.bat
@@ -1,1 +1,1 @@
-copy Date.jsp C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT\KobiKuzi
+copy Date.jsp "C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT\KobiKuzi"
