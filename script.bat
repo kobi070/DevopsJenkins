1a9d07e4f9c7e94c01d184aeee9aa25e7963a0fa
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 04:01:15 +0200

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,16 +1,11 @@
 
 
-# move to Tomcat 10.0 ROOT
 cd C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT
 
-#print the route
 echo $ C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT
 
-# print again
 echo %cd%
 
-# create a new dir in the ROOT folder 
 md KobiKuzi
 
-# show new dir
 dir
