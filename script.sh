0dc476d390ea4d6db2883b7320eacddb408026a5
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 03:05:42 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# move to Tomcat 10.0 ROOT
+cd C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT
+
+#print the route
+echo $ C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT
+
+# create a new dir in the ROOT folder 
+mkdir KobiKuzi
+
+# show new dir
+dir C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\ROOT
