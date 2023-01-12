ea920285de61253ea964b61999e74221a49be2cb
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 04:08:39 +0200

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,11 +1,2 @@
-
-
 cd C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT
-
-echo $ C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT
-
-echo %cd%
-
 md KobiKuzi
-
-dir
