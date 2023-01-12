68cda90a0cf9d42856bd5d211585dc02a331538c
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 03:13:51 +0200

    Update and rename script.sh to script.bat

---
+++ script.bat
@@ -0,0 +1,16 @@
+
+
+# move to Tomcat 10.0 ROOT
+cd C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT
+
+#print the route
+echo $ C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT
+
+# print again
+echo %cd%
+
+# create a new dir in the ROOT folder 
+md KobiKuzi
+
+# show new dir
+dir
