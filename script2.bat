8b26844f4f0b2b11d48aa9c3f0348f20e4a4a301
kobi070 <93041042+kobi070@users.noreply.github.com>
2023-01-12 04:01:50 +0200

    Update script2.bat

--- script2.bat
+++ script2.bat
@@ -1,7 +1,7 @@
-# move to JOB_6_GIT_1
+
 cd C:/Users/Kobi/.jenkins/workspace/JOB_6_GIT_1
 
-# copy Date.jsp
+
 copy C:/Users/Kobi/.jenkins/workspace/JOB_6_GIT_1/Date.jsp C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT/KobiKuzi
 
 cd C:/Program Files/Apache Software Foundation/Tomcat 10.0/webapps/ROOT/KobiKuzi
